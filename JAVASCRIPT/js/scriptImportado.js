dd7c5246941f377a83d6717bdd38fe13d403c8f5
arbitrapp <josemariamonzon@hotmail.com>
2025-09-29 02:04:49 -0300

    modificacion profesionales 1

---
+++ JAVASCRIPT/js/scriptImportado.js
@@ -0,0 +1,12 @@
+let nombre = 'Nacho';
+document.write('Hola ' + nombre + "<br/>");
+nombre = 'Analía';
+document.write(`Hola ${nombre}`);
+
+// Comentarios de una línea
+/* Comentarios
+de varias líneas */
+alert('Hola ' + nombre);
+alert(`Hola ${nombre}`);
+console.log('Hola ' + nombre);
+console.log(`Hola ${nombre}`);  
